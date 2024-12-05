f9e2035a543d0ebf8322eda055678ae3414dfae0
axnah <anna.nieto@efrei.net>
2024-12-05 20:38:51 +0100

    first commit init projet

---
+++ hopehope/src/main/java/altn72/hopehope/HopeHopeApplication.java
@@ -0,0 +1,13 @@
+package altn72.hopehope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeHopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeHopeApplication.class, args);
+	}
+
+}
---
+++ hopehope/src/test/java/altn72/hopehope/HopeHopeApplicationTests.java
@@ -0,0 +1,13 @@
+package altn72.hopehope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeHopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
