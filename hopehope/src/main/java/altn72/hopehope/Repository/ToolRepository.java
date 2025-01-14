755422ba039dbdc9136beadb863c1aa147654f3c
mariusefrei <marius.cahagne@efrei.net>
2025-01-14 20:26:38 +0100

    fix ToolRepository

--- hopehope/src/main/java/altn72/hopehope/Repository/ToolRepository.java
+++ hopehope/src/main/java/altn72/hopehope/Repository/ToolRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface ToolRepository extends JpaRepository<Tool, Long> {
     @Query("SELECT t FROM Tool t WHERE t.id = :Id")
-    Optional(Tool) findById(String Id);
+    Optional<Tool> findById(String Id);
     @Query("SELECT t FROM Tool t WHERE LOWER(t.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(t.domain) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Tool> searchByKeyword(String keyword);
 }
