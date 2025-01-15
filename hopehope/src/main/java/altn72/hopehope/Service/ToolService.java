8eb9e86cfbb597950d4d3b4271db35cf5282b545
realmhaven <52790871+realmhaven@users.noreply.github.com>
2025-01-15 19:10:42 +0100

    Added missing methods to persist data in toolService

--- hopehope/src/main/java/altn72/hopehope/Service/ToolService.java
+++ hopehope/src/main/java/altn72/hopehope/Service/ToolService.java
@@ -4,8 +4,11 @@ import altn72.hopehope.Model.Tool;
 import altn72.hopehope.Repository.ToolRepository;
 import altn72.hopehope.Dto.ToolDTO;
 import altn72.hopehope.Mapper.ToolMapper;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +19,16 @@ public class ToolService {
 
     @Autowired
     private ToolRepository toolRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void saveAll(List<Tool> tools) {
+        for (Tool tool : tools) {
+            entityManager.persist(tool);
+        }
+    }
 
     public List<ToolDTO> getAllTools() {
         return toolRepository.findAll()
