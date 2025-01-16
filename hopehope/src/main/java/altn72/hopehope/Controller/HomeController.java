5f72f388f42dee9c064c2ce2f6bc741a90781acf
realmhaven <52790871+realmhaven@users.noreply.github.com>
2025-01-16 08:53:19 +0100

    ajout du controller home

---
+++ hopehope/src/main/java/altn72/hopehope/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package altn72.hopehope.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/main")
+    String getMain(Model model) {
+        return "main";
+    }
+    @GetMapping("/bonjour")
+    String getBonjour(Model model) {
+        return "bonjour";
+    }
+
+
+}
